aaddfe79b11da76787f7d3ca39af573c2ce450ed
Kyle Liebenberg <kyle.liebenberg@labs.epiuse.com>
2026-04-05 00:56:17 +0200

    ci: setup initial repo for spring boot

---
+++ backend/src/test/java/za/co/tuks/amrdashboard/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.tuks.amrdashboard.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
